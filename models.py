a02845ed8b4c7c87b263f1e33eea2054e43f1990
sibapybot <sibabot.python@gmail.com>
2024-07-15 16:09:19 +0900

    :hammer: 文字数制限の追加

---
+++ models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(15), unique=True, index=True)
+    name = Column(String(32))
+    email = Column(String(150), unique=True, index=True)
+    hashed_password = Column(String)
+
+    created_at = Column(DateTime, default=datetime.now("UTC"))
+
+
+
+
