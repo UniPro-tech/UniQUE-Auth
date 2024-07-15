91d8167c386d13aaac15815da5272857338f5c74
sibapybot <sibabot.python@gmail.com>
2024-07-15 17:23:32 +0900

    :recycle: pylintくんよしよし

--- crud.py
+++ crud.py
@@ -1,35 +1,35 @@
 import hashlib
 from sqlalchemy.orm import Session
-from .schemas import UserCreate, User
-from .models import User as UserModel
+
+from . import models, schemas
 
 
-def get_user(db: Session, user_id: int) -> User:
-    return db.query(UserModel).filter(UserModel.id == user_id).first()
+def get_user(db: Session, user_id: int) -> schemas.User:
+    return db.query(models.User).filter(models.User.id == user_id).first()
 
 
-def get_user_by_email(db: Session, email: str) -> User:
-    return db.query(UserModel).filter(UserModel.email == email).first()
+def get_user_by_email(db: Session, email: str) -> schemas.User:
+    return db.query(models.User).filter(models.User.email == email).first()
 
 
-def get_user_by_userid(db: Session, user_id: str) -> User:
-    return db.query(UserModel).filter(UserModel.user_id == user_id).first()
+def get_user_by_userid(db: Session, user_id: str) -> schemas.User:
+    return db.query(models.User).filter(models.User.user_id == user_id).first()
 
 
 def get_user_by_email_password(
             db: Session, email: str, password: str
-        ) -> User:
+        ) -> schemas.User:
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
-    return (db.query(UserModel)
-            .filter(UserModel.email == email,
-                    UserModel.hashed_password == hashed_password
+    return (db.query(models.User)
+            .filter(models.User.email == email,
+                    models.User.hashed_password == hashed_password
                     )
             .first())
 
 
-def create_user(db: Session, user: UserCreate) -> User:
+def create_user(db: Session, user: schemas.UserCreate) -> schemas.User:
     hashed_password = hashlib.sha256(user.password.encode()).hexdigest()
-    db_user = UserModel(email=user.email, hashed_password=hashed_password)
+    db_user = models.User(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
