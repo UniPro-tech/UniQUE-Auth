7920e2055a660afdd03dfc813e954108e907cb04
sibapybot <sibabot.python@gmail.com>
2024-07-15 16:11:41 +0900

    :sparkles: add new feature

---
+++ schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+class UserBase(BaseModel):
+    name: str = Field(..., min_length=1, max_length=32, regex=r'^[a-zA-Z0-9_]+$')
+    email: str = Field(..., min_length=1, max_length=15, regex=r'/^[a-z\d][\w.-]*@[\w.-]+\.[a-z\d]+$/i')
+
+
+class UserCreate(UserBase):
+    password: str = Field(..., min_length=8, max_length=30, regex=r'/^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,20}$/i')
+
+
+class User(UserBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
