b9d61c5843664add78d0ea44aede4f4a5968a81f
sibapybot <sibabot.python@gmail.com>
2024-07-15 16:33:02 +0900

    :hammer: 返却する型を指定

--- crud.py
+++ crud.py
@@ -5,21 +5,21 @@ import re
 from . import models, schemas
 
 
-def get_user(db: Session, user_id: int):
+def get_user(db: Session, user_id: int) -> schemas.User:
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 
-def get_user_by_email(db: Session, email: str):
+def get_user_by_email(db: Session, email: str) -> schemas.User:
     return db.query(models.User).filter(models.User.email == email).first()
 
-def get_user_by_email(db: Session, name: str):
-    return db.query(models.User).filter(models.User.name == name).first()
+def get_user_by_email(db: Session, user_id: str) -> schemas.User:
+    return db.query(models.User).filter(models.User.user_id == user_id).first()
 
-def get_user_by_email_password(db: Session, email: str, password: str):
+def get_user_by_email_password(db: Session, email: str, password: str) -> schemas.User:
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     return db.query(models.User).filter(models.User.email == email, models.User.hashed_password == hashed_password).first()
 
-def create_user(db: Session, user: schemas.UserCreate):
+def create_user(db: Session, user: schemas.UserCreate) -> schemas.User:
     hashed_password = hashlib.sha256(user.password.encode()).hexdigest()
     db_user = models.User(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
