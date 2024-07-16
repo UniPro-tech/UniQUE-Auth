dad2552e0a77ef133a9a2a8d86d0ec6e84019836
sibapybot <sibabot.python@gmail.com>
2024-07-16 20:36:22 +0900

    :hammer: 正規表現の修正

--- schemas.py
+++ schemas.py
@@ -4,17 +4,18 @@ from pydantic import BaseModel, Field
 
 class UserBase(BaseModel):
     user_id: str = Field(...,
-                         min_length=1, max_length=15, regex=r'^[a-zA-Z0-9_]+$'
+                         min_length=1, max_length=15,
+                         pattern=r'^[a-zA-Z0-9_]+$'
                          )
     email: str = Field(...,
-                       regex=r'/^[a-z\d][\w.-]*@[\w.-]+\.[a-z\d]+$/i'
+                       pattern=r'^[a-z\d][\w.-]*@[\w.-]+\.[a-z\d]+$/i'
                        )
 
 
 class UserCreate(UserBase):
     password: str = Field(...,
                           min_length=8, max_length=30,
-                          regex=r'/^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,20}$/i'
+                          pattern=r'^[a-zA-Z0-9_@\-!?]+$',
                           )
 
 
@@ -22,9 +23,9 @@ class User(UserBase):
     id: int
     name: str = Field(...,
                       min_length=1, max_length=32,
-                      regex=r'^[a-zA-Z0-9_]+$'
+                      pattern=r'^[a-zA-Z0-9_]+$'
                       )
     created_at: datetime
 
     class Config:
-        orm_mode = True
+        from_attributes = True
