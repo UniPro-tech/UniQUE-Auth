9d2184aa2dc7cb88881f4c0e98a3620afcd7ddbf
sibapybot <sibabot.python@gmail.com>
2024-07-20 08:52:57 +0900

    :hammer: エンドポイントの追加

--- app/routers/token/__init__.py
+++ app/routers/token/__init__.py
@@ -3,4 +3,18 @@ from sqlalchemy.orm import Session
 from ... import crud, models, schemas
 from ...database import get_db
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/token",
+    tags=["token"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/", response_model=schemas.Token)
+async def create_token(db: Session = Depends(get_db)):
+    pass
+
+
+@router.delete("/", response_model=schemas.Token)
+async def delete_token(db: Session = Depends(get_db)):
+    pass
--- app/routers/token/code.py
+++ app/routers/token/code.py
@@ -3,4 +3,13 @@ from sqlalchemy.orm import Session
 from ... import crud, models, schemas
 from ...database import get_db
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/token/code",
+    tags=["token/code"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/", response_model=schemas.Token)
+async def create_code(db: Session = Depends(get_db)):
+    pass
