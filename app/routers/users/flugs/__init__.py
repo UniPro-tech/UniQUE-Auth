e5b6bbd22a4a34eed9b6a988a15b1e4547bedc73
sibapybot <sibabot.python@gmail.com>
2024-07-19 21:35:04 +0900

    :hammer: routerの追加

--- app/routers/users/flugs/__init__.py
+++ app/routers/users/flugs/__init__.py
@@ -2,3 +2,5 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from .... import crud, models, schemas
 from ....database import get_db
+
+router = APIRouter()
