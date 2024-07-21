357929c8a74e9b34fad9ee803ffa53d9bd21c285
sibapybot <sibabot.python@gmail.com>
2024-07-21 16:05:06 +0900

    :sparkles: token生成部分を追加

---
+++ app/models/token.py
@@ -0,0 +1,40 @@
+import os
+from typing import List
+import time
+import jwt
+from dotenv import load_dotenv
+
+# .envファイルの内容を読み込見込む
+load_dotenv(dotenv_path='../../.env')
+
+
+__SQLALCHEMY_DATABASE_URI = os.environ['SECRET_KEY']
+
+
+def generate_token(
+            payload: dict,
+            header: dict = {
+                "alg": "HS256",
+                "typ": "JWT"}
+            ):
+    global __SQLALCHEMY_DATABASE_URI
+    encoded_jwt = jwt.encode(
+        payload=payload, key=__SQLALCHEMY_DATABASE_URI,
+        algorithm=header["alg"], headers=header
+        )
+    return encoded_jwt
+
+
+def generate_access_token(
+        client_id: int,
+        scope: List[str], user_id: int
+        ):
+    payload = {
+        'iss': 'https://portal.uniproject-tech.net',
+        'sub': client_id,
+        'user': user_id,
+        'exp': int(time.time()) + 604800,
+        'scope': scope
+    }
+
+    return generate_token(payload=payload)
