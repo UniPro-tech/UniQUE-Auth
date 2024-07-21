10d1d6ce80859fa9f1aeb83b3d452c03e8d33865
sibapybot <sibabot.python@gmail.com>
2024-07-21 21:22:53 +0900

    :hammer: import周りの修正

--- app/cruds/token.py
+++ app/cruds/token.py
@@ -1,11 +1,11 @@
 from sqlalchemy.orm import Session
 import schemas
 import schemas.token
-from ..models.token import generate_access_token, generate_refresh_token
+from ..utils.token import generate_access_token, generate_refresh_token, verified_token
 
 
 def create_access_token(
-        db: Session, token: schemas.token.AccessToken
+        db: Session, token: schemas.AccessToken
         ) -> str:
 
     access_token = generate_access_token(
@@ -18,7 +18,7 @@ def create_access_token(
 
 
 def create_refresh_token(
-        db: Session, token: schemas.token.RefreshToken
+        db: Session, token: schemas.RefreshToken
         ) -> str:
 
     refresh_token = generate_refresh_token(
@@ -26,3 +26,12 @@ def create_refresh_token(
                                 exp=token.exp
                                 )
     return refresh_token
+
+
+def validate_access_token(
+        db: Session, token: str
+        ) -> bool:
+
+    decode_token = verified_token(token=token)
+    if decode_token:
+        
