733ec6b46e9789769d79103d74681e2eb884a730
sibapybot <sibabot.python@gmail.com>
2024-07-21 21:19:19 +0900

    :sparkles: import周りの改善

---
+++ app/models/__init__.py
@@ -0,0 +1,10 @@
+from .app import App
+from .user import User
+from .role import Role
+
+
+__all__ = [
+    "App",
+    "User",
+    "Role",
+]
---
+++ app/schemas/__init__.py
@@ -0,0 +1,20 @@
+from .user import (
+    User,
+    UserCreate,
+    Me,
+    Admin
+)
+from .token import (
+    AccessToken,
+    RefreshToken
+)
+
+
+__all__ = [
+    "User",
+    "UserCreate",
+    "Me",
+    "Admin",
+    "AccessToken",
+    "RefreshToken",
+]
