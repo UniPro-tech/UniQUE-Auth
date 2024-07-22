e73286830a9fc56a7d97905db7583154fe48d412
sibapybot <sibabot.python@gmail.com>
2024-07-22 20:45:39 +0900

    :sparkles: 中間テーブルファイルの追加

---
+++ app/models/middle_table.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Table, ForeignKey, Column, Integer
+from ..database import Base
+
+# UserとRoleの多対多リレーション
+user_roles = Table(
+    'user_roles',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id'), primary_key=True),
+    Column('role_id', Integer, ForeignKey('roles.id'), primary_key=True)
+)
+
+# UserとFlagの多対多リレーション
+user_flags = Table(
+    'user_flags',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id'), primary_key=True),
+    Column('flag_id', Integer, ForeignKey('flags.id'), primary_key=True)
+)
+
+# UserとAppの多対多リレーション
+user_apps = Table(
+    'user_apps',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id'), primary_key=True),
+    Column('app_id', Integer, ForeignKey('apps.id'), primary_key=True)
+)
+
+# AppとFlagの多対多リレーション
+app_flags = Table(
+    'app_flags',
+    Base.metadata,
+    Column('app_id', Integer, ForeignKey('apps.id'), primary_key=True),
+    Column('flag_id', Integer, ForeignKey('flags.id'), primary_key=True)
+)
