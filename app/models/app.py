73028537c1247d854cadcb71f6f844a4383448d3
sibapybot <sibabot.python@gmail.com>
2024-07-23 22:33:45 +0900

    :recycle: リファクタリング

--- app/models/app.py
+++ app/models/app.py
@@ -8,17 +8,14 @@ class App(Base):
     __tablename__ = "apps"
 
     id = Column(Integer, primary_key=True, index=True)
+    client_id = Column(String, index=True)
     name = Column(String, index=True)
     description = Column(String, index=True)
     owner_id = Column(Integer, index=True)
-    users = Column(String, index=True)
     created_at = Column(Integer)
     updated_at = Column(Integer)
     deleted_at = Column(Integer)
-    is_deleted = Column(Boolean, default=False)
-    is_active = Column(Boolean, default=True)
     is_locked = Column(Boolean, default=False)
-    is_banned = Column(Boolean, default=False)
 
     # 多対多リレーション
     users = relationship('User', secondary=user_apps, back_populates='apps')
