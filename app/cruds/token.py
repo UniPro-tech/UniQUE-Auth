78a2b3d43cc109a72fe85d5478267633128665a3
sibapybot <sibabot.python@gmail.com>
2024-08-04 20:04:18 +0900

    :hammer: ペイロードの変更に伴う修正

--- app/cruds/token.py
+++ app/cruds/token.py
@@ -1,18 +1,24 @@
 from sqlalchemy.orm import Session
+from uuid_extensions import uuid7str
 import schemas
 import schemas.token
-from ..utils.token import generate_access_token, generate_refresh_token, verified_token
+from ..utils.token import (
+    generate_access_token, generate_refresh_token,
+    verified_token
+    )
 
 
 def create_access_token(
         db: Session, token: schemas.AccessToken
         ) -> str:
-
+    uuid = uuid7str()
+    # TODO:トークンをDBに保存する処理を追加する
     access_token = generate_access_token(
-                                client_id=token.sub,
+                                client_user_id=token.client_user_id,
+                                client_app_id=token.client_app_id,
                                 scope=token.scope,
-                                user_id=token.user,
-                                exp=token.exp
+                                exp=token.exp,
+                                uuid=uuid
                                 )
     return access_token
 
@@ -20,10 +26,14 @@ def create_access_token(
 def create_refresh_token(
         db: Session, token: schemas.RefreshToken
         ) -> str:
-
+    uuid = uuid7str()
+    # TODO:トークンをDBに保存する処理を追加する
     refresh_token = generate_refresh_token(
                                 token_id=token.token_id,
-                                exp=token.exp
+                                exp=token.exp,
+                                client_user_id=token.client_user_id,
+                                client_app_id=token.client_app_id,
+                                uuid=uuid,
                                 )
     return refresh_token
 
