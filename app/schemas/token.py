e741451195fd158c10ae9a64fd1f65fbb217c43b
sibapybot <sibabot.python@gmail.com>
2024-08-06 20:52:52 +0900

    :memo: コメントの追加

--- app/schemas/token.py
+++ app/schemas/token.py
@@ -5,6 +5,22 @@ from nanoid import generate
 
 
 class BaseToken(BaseModel):
+    """
+    UniQUEのトークンの基底クラス
+    Args:
+        <alg> <str>:
+                暗号化アルゴリズム(UniQUEではHS256のみ)
+        <typ> <str>:
+                トークンの種類(UniQUEではJWTのみ)
+        <iss> <str>:
+                発行者(UniQUEではhttps://portal.uniproject-tech.net)
+        <jti> <int>:
+                トークンID
+        <sub> <int>:
+                クライアントアプリケーションID
+        <for_> <int>:
+                クライアントユーザーID
+    """
     alg: str = Field(default="HS256")
     typ: str = Field(default="JWT")
     iss: str = Field(default="https://portal.uniproject-tech.net")
@@ -14,6 +30,14 @@ class BaseToken(BaseModel):
 
 
 class AccessToken(BaseToken):
+    """
+    AccessTokenのスキーマ
+    Args:
+        <scope> <List[str]>:
+                パーミッション
+        <exp> <int>:
+                有効期限(AccessTokenは7日間)
+    """
     scope: List[str] = Field(..., description="permissions")
     exp: int = Field(default=time.time() + 604800, description="有効期限")
 
@@ -22,6 +46,12 @@ class AccessToken(BaseToken):
 
 
 class RefreshToken(BaseToken):
+    """
+    RefreshTokenのスキーマ
+    Args:
+        <exp> <int>:
+                有効期限(RefreshTokenは14日間)
+    """
     exp: int = Field(default=time.time() + 1209600, description="有効期限")
 
     class Config:
