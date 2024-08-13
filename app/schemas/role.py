aab593df226c2547b81ad45ec92756ef1f16b914
sibapybot <sibabot.python@gmail.com>
2024-08-13 22:45:30 +0900

    :hammer: pydanticのクラス作成

---
+++ app/schemas/role.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+
+class RoleBase(BaseModel):
+    id:  int
+    name: str = Field(..., max_length=100)
+    parmission_bit: int = Field(default=0)
+    sort: int = Field(default=0)
+
+
+class Role(RoleBase):
+    description: str = Field(..., max_length=255)
+    # 多体多のリレーション
+    users: List[int] = Field(default=[])
+    logs: List[int] = Field(default=[])
+
+    class Config:
+        orm_mode = True
+
+
+class CreateRole(RoleBase):
+    description: str = Field(..., max_length=255)
+
+    class Config:
+        orm_mode = True
