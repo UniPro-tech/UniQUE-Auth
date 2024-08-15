37b486e3ec3de57ec3a48ddea647fd900b0249fe
sibapybot <sibabot.python@gmail.com>
2024-08-15 21:28:48 +0900

    :sparkles: Userの想定される処理一通りを実装

--- app/cruds/user.py
+++ app/cruds/user.py
@@ -1,16 +1,46 @@
-from ..models import User
-from ..schemas import User
+from ..models import User as UserModel
+from ..schemas import User as UserSchema
+from ..schemas import CreateUser as CreateUserSchema
 
 
-def get_user_by_email(db, email):
-    user = db.query(User).filter(User.email == email).first()
-    return User.model_validate(user) if user else None
-
-def get_user_by_id(db, user_id):
-    return db.query(User).filter(User.id == user_id).first()
+async def get_user_by_email(db, email):
+    user = db.query(UserModel).filter(UserModel.email == email).first()
+    return UserSchema.model_validate(user) if user else None
 
 
-def get_user_by_id(db, user_id):
-    return db.query(User).filter(User.id == user_id).first()
+async def get_user_by_id(db, user_id):
+    user = db.query(UserModel).filter(UserModel.id == user_id).first()
+    return UserSchema.model_validate(user) if user else None
 
 
+async def get_users(db, skip: int = 0, limit: int = 100):
+    # WARNING: この関数を使用するユーザーは制限すること。
+    users = db.query(UserModel).offset(skip).limit(limit).all()
+    return [UserSchema.model_validate(user) for user in users]
+
+
+async def create_user(db, user: CreateUserSchema):
+    user = UserModel(**user.model_dump())
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return UserSchema.model_validate(user)
+
+
+async def update_user(db, user_id: int, user: CreateUserSchema):
+    user = db.query(UserModel).filter(UserModel.id == user_id).first()
+    if user:
+        user.update(**user.model_dump())
+        db.commit()
+        db.refresh(user)
+        return UserSchema.model_validate(user)
+    return None
+
+
+async def delete_user(db, user_id: int):
+    user = db.query(UserModel).filter(UserModel.id == user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+        return UserSchema.model_validate(user)
+    return None
