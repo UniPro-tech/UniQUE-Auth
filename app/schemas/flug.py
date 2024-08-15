cbefe16cab64d438826af0b89c020aa1b78d70e1
sibapybot <sibabot.python@gmail.com>
2024-08-15 10:00:44 +0900

    :sparkles: Flag型を定義

--- app/schemas/flug.py
+++ app/schemas/flug.py
@@ -7,5 +7,23 @@ class BaseFlug(BaseModel):
     """
     id: int
     name: str = Field(..., min_length=1, max_length=32)
+    users: list[int] = Field(default=[])
+
+
+class Flug(BaseFlug):
+    """
+    Flugの詳細クラス
+    """
     description: str = Field(..., max_length=1024)
     image_url: str = Field(..., max_length=256)
+    users: list[int] = Field(default=[])
+    logs: list[int] = Field(default=[])
+
+
+class CreateFlug(BaseFlug):
+    """
+    Flugの作成クラス
+    """
+
+    class Config:
+        orm_mode = True
