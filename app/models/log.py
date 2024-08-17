020e1eed70a612e8636754e46c5efa61e73771ce
sibapybot <sibabot.python@gmail.com>
2024-08-17 23:11:55 +0900

    :rewind: LogDBテーブルの分割

--- app/models/log.py
+++ app/models/log.py
@@ -1,5 +1,5 @@
 import time
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from ..database import Base
 
@@ -9,18 +9,67 @@ class Log(Base):
 
     log_id = Column(Integer, primary_key=True, autoincrement=True)
     action_type = Column(String(50), nullable=False)
-    user_id = Column(
-        Integer, ForeignKey('users.id'), nullable=True, index=True
-        )
-    app_id = Column(
-        Integer, ForeignKey('apps.id'), nullable=True, index=True
-        )
-    flag_id = Column(
-        Integer, ForeignKey('flags.id'), nullable=True, index=True
-        )
+    user_id = Column(Integer, nullable=False, index=True)
+    app_id = Column(Integer, nullable=False, index=True)
     details = Column(String(255))
     timestamp = Column(Integer, default=int(time.time()))
 
+
+class UserLog(Base):
+    __tablename__ = 'user_logs'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    action = Column(String(50), nullable=False)
+    action_at = Column(Integer, default=int(time.time()))
+    description = Column(String(255))
+    ipadress = Column(String(50))
+
     user = relationship('User', back_populates='logs')
-    app = relationship('App', back_populates='logs')
+
+
+class RoleLog(Base):
+    __tablename__ = 'role_logs'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    action = Column(String(50), nullable=False)
+    action_at = Column(Integer, default=int(time.time()))
+    action_user = Column(Integer, nullable=False)
+    description = Column(String(255))
+
+    role = relationship('Role', back_populates='logs')
+
+
+class FlagLog(Base):
+    __tablename__ = 'flag_logs'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    action = Column(String(50), nullable=False)
+    action_at = Column(Integer, default=int(time.time()))
+    action_user = Column(Integer, nullable=False)
+    description = Column(String(255))
+
     flag = relationship('Flag', back_populates='logs')
+
+
+class AppLog(Base):
+    __tablename__ = 'app_logs'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    action = Column(String(50), nullable=False)
+    action_at = Column(Integer, default=int(time.time()))
+    action_user = Column(Integer, nullable=False)
+    description = Column(String(255))
+
+    app = relationship('App', back_populates='logs')
+
+
+class ClientLog(Base):
+    __tablename__ = 'client_logs'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    action = Column(String(50), nullable=False)
+    action_at = Column(Integer, default=int(time.time()))
+    action_user = Column(Integer, nullable=False)
+    description = Column(String(255))
+
+    client = relationship('Client', back_populates='logs')
