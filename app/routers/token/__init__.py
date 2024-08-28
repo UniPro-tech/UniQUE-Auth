d510c656c8c6ff8b2399a0144ad781de2210367d
sibapybot <sibabot.python@gmail.com>
2024-08-28 21:50:39 +0900

    :hammer: 不必要なimportの削除及び過剰な例外処理の削除

--- app/routers/token/__init__.py
+++ app/routers/token/__init__.py
@@ -2,8 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from ... import schemas
 from ...cruds.token import (
-    create_access_token, create_refresh_token,
-    add_db_token, recrate_token
+    recrate_token
 )
 from ...cruds.client import create_user_client
 from ...cruds.user import get_user_by_email_passwd
@@ -24,31 +23,29 @@ async def create_token(
             email: str, passwd: str, scope: list[str],
             db: Session = Depends(get_db)
         ):
-    try:
-        user: schemas.User | False = get_user_by_email_passwd(
-            db, email, passwd
+    user: schemas.User | False = get_user_by_email_passwd(
+        db, email, passwd
+    )
+
+    if user:
+        user_client_data = schemas.UserClient(
+            user=user.id
+        )
+        user_client: schemas.UserClient = create_user_client(
+            db, user_client_data
         )
 
-        if user:
-            user_client_data = schemas.UserClient(
-                user=user.id
-            )
-            user_client: schemas.UserClient = create_user_client(
-                db, user_client_data
-            )
-            permissionsbit = generate_permissionbit(*scope)
+        permissionsbit = generate_permissionbit(*scope)
+        _, recrate_token_data = await recrate_token(
+            user.id, user_client.id, permissionsbit, db
+        )
+        return {
+                "code": recrate_token_data[1],
+            }
 
-            _, recrate_token_data = await recrate_token(
-                user.id, user_client.id, permissionsbit, db
-            )
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
 
-            return {
-                    "code": recrate_token_data[1],
-                }
-        else:
-            raise HTTPException(status_code=404, detail="User not found")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="server error")
 
 
 @router.get("/", response_model=schemas.Token)
