135d23f1c2202faf18170b60e61a2529b7dbdfd5
sibapybot <sibabot.python@gmail.com>
2024-08-28 21:47:28 +0900

    :hammer: ログイン処理の最適化

--- app/routers/token/__init__.py
+++ app/routers/token/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from ... import schemas
-from ...cruds.token import create_access_token, create_refresh_token
+from ...cruds.token import (
+    create_access_token, create_refresh_token,
+    add_db_token, recrate_token
+)
 from ...cruds.client import create_user_client
 from ...cruds.user import get_user_by_email_passwd
 from ...database import get_db
@@ -25,28 +28,31 @@ async def create_token(
         user: schemas.User | False = get_user_by_email_passwd(
             db, email, passwd
         )
-        permissionsbit = generate_permissionbit(*scope)
+
         if user:
-            client: schemas.UserClient = create_user_client(db, user)
-            access_token = schemas.AccessToken(
-                sub=user.id,
-                iss=client.id,
-                scope=permissionsbit
+            user_client_data = schemas.UserClient(
+                user=user.id
             )
-            access_token = create_access_token(user)
-            refresh_token = create_refresh_token(user)
+            user_client: schemas.UserClient = create_user_client(
+                db, user_client_data
+            )
+            permissionsbit = generate_permissionbit(*scope)
+
+            _, recrate_token_data = await recrate_token(
+                user.id, user_client.id, permissionsbit, db
+            )
+
             return {
-                    "access_token": access_token,
-                    "refresh_token": refresh_token
+                    "code": recrate_token_data[1],
                 }
         else:
             raise HTTPException(status_code=404, detail="User not found")
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail="server error")
 
 
 @router.get("/", response_model=schemas.Token)
-async def verify_token(db: Session = Depends(get_db)):
+async def verify_token(token: str, db: Session = Depends(get_db)):
     pass
 
 
