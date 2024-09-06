4577ba314b13b1c082469e15a37759b14c99040a
sibapybot <sibabot.python@gmail.com>
2024-09-06 18:56:15 +0900

    :hammer: ユーザーの作成処理を追加

--- app/routers/users/__init__.py
+++ app/routers/users/__init__.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from ... import crud, models, schemas
+from ... import schemas
+from ...cruds.user import (
+    create_user,
+    get_user_by_name,
+    get_user_by_email
+)
 from ...database import get_db
 
 router = APIRouter(
@@ -8,6 +13,16 @@ router = APIRouter(
 )
 
 
-@router.post("/")
-async def create_link_apps(db: Session = Depends(get_db)):
-    pass
+@router.post("/", response_model=schemas.User)
+async def user_create(user: schemas.CreateUser, db: Session = Depends(get_db)):
+
+    serch_user = await get_user_by_name(db, user.name)
+    serch_user = await get_user_by_email(db, user.email)
+    if serch_user:
+        raise HTTPException(status_code=400, detail="User already registered")
+    if serch_user:
+        raise HTTPException(status_code=400, detail="User already registered")
+
+    user = await create_user(db, user)
+
+    return user
