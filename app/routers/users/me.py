b0e2cae025302a16d1d9464f176f8782f4ea14c7
sibapybot <sibabot.python@gmail.com>
2024-09-11 21:35:21 +0900

    :sparkles: 自分のユーザー情報を取得する処理を追加

--- app/routers/users/me.py
+++ app/routers/users/me.py
@@ -2,19 +2,19 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from ... import crud, models, schemas
 from ...database import get_db
-from ...dependencies import verify_token
+from ...cruds.token import verify_token
 
 router = APIRouter(
     prefix="/users/me",
     tags=["users"],
-    dependencies=[Depends(verify_token)],
+    dependencies=[],
     responses={404: {"description": "Not found"}},
 )
 
 
-@router.get("/", response_model=schemas.User)
-async def read_users_me(db: Session = Depends(get_db)):
-    pass
+@router.get("/", response_model=schemas.Me)
+async def read_users_me(user: schemas.Me = Depends(verify_token())):
+    return user
 
 
 @router.patch("/", response_model=schemas.User)
