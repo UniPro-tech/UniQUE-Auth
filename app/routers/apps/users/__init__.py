c8cae6daeae06bcaacc0f466548d068c64562525
sibapybot <sibabot.python@gmail.com>
2024-09-15 23:13:53 +0900

    :sparkles: appのユーザーリスト取得APIを追加

--- app/routers/apps/users/__init__.py
+++ app/routers/apps/users/__init__.py
@@ -1,7 +1,14 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from .... import crud, models, schemas
 from ....database import get_db
+from ....cruds.token import verify_token
+from ....schemas import (
+    Me as MeSchema,
+    User as UserSchema,
+    App as AppSchema,
+)
+from ....cruds.app import get_app_by_id
 
 router = APIRouter(
     prefix="/apps",
@@ -9,6 +16,18 @@ router = APIRouter(
 )
 
 
-@router.get("/{app_id}/users", response_model=schemas.User)
-async def read_users(app_id: int, db: Session = Depends(get_db)):
-    return crud.get_users_by_app_id(db, app_id)
+@router.get("/{app_id}/users", response_model=List[UserSchema])
+async def read_users(
+            app_id: int,
+            user: MeSchema = Depends(verify_token),
+            db: Session = Depends(get_db)
+        ):
+    app: AppSchema = get_app_by_id(db, app_id)
+    if not app:
+        raise HTTPException(status_code=404, detail="App not found")
+    elif app.is_enable is False:
+        raise HTTPException(status_code=404, detail="App is not enable")
+    elif app.admin_users not in user.id:
+        raise HTTPException(status_code=403, detail="Permission denied")
+
+    return app.users
