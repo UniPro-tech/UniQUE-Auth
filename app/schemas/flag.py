c4b04a897771e88b63955b8ba9d0e1e6929bdb7c
sibapybot <sibabot.python@gmail.com>
2024-09-17 22:08:39 +0900

    :hammer: スキーマを変更

--- app/schemas/flag.py
+++ app/schemas/flag.py
@@ -6,19 +6,21 @@ class BaseFlag(BaseModel):
     """
     flagの基底クラス
     """
-    id: int
     name: str = Field(..., min_length=1, max_length=32)
-    users: List[int] = Field(default=[])
 
 
 class Flag(BaseFlag):
     """
     flagの詳細クラス
     """
+    id: int
     description: str = Field(..., max_length=1024)
     image_url: str = Field(..., max_length=256)
     users: List[int] = Field(default=[])
     logs: List[int] = Field(default=[])
+
+    class Config:
+        model_validate = True
 
 
 class CreateFlag(BaseFlag):
