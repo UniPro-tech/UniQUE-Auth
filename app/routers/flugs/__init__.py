9d344874228adb90207a352c93fe2000d755bcc9
sibapybot <sibabot.python@gmail.com>
2024-09-17 22:09:10 +0900

    :hammer: 関数の呼び出し周りのリファクタリング＆修正

--- app/routers/flugs/__init__.py
+++ app/routers/flugs/__init__.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from ... import crud, models, schemas
+from ...schemas import (
+    Me as MeSchema,
+    Flag as FlagSchema,
+    CreateFlag as CreateFlagSchema,
+)
+from ...cruds.token import verify_token
 from ...database import get_db
 
 router = APIRouter(
@@ -9,21 +15,26 @@ router = APIRouter(
 )
 
 
-@router.post("/", response_model=schemas.Flug)
-async def create_flug(flug: schemas.FlugCreate, db: Session = Depends(get_db)):
+@router.post("/", response_model=FlagSchema)
+async def create_flug(
+            flug: CreateFlagSchema,
+            user: MeSchema = Depends(verify_token),
+            db: Session = Depends(get_db)
+        ):
+    
     pass
 
 
-@router.get("/{flug_id}", response_model=schemas.Flug)
+@router.get("/{flug_id}", response_model=FlagSchema)
 async def read_flug(flug_id: int, db: Session = Depends(get_db)):
     pass
 
 
-@router.patch("/{flug_id}", response_model=schemas.Flug)
+@router.patch("/{flug_id}", response_model=FlagSchema)
 async def edit_flug(flug_id: int, db: Session = Depends(get_db)):
     pass
 
 
-@router.delete("/{flug_id}", response_model=schemas.Flug)
+@router.delete("/{flug_id}", response_model=FlagSchema)
 async def delete_flug(flug_id: int, db: Session = Depends(get_db)):
     pass
