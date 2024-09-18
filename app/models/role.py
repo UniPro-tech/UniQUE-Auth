c33eb700b3f804d2581ae25ab9df6c041c48aed0
sibapybot <sibabot.python@gmail.com>
2024-09-18 22:01:49 +0900

    :hammer: 新しい値を追加

--- app/models/role.py
+++ app/models/role.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 from ..database import Base
 from .middle_table import user_roles
@@ -9,8 +9,11 @@ class Role(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
+    display_name = Column(String, index=True)
     description = Column(String, index=True)
     permission_bit = Column(Integer, default=0)
+    is_enabled = Column(Boolean, default=True)
+    is_deactivated = Column(Boolean, default=False)
     sort = Column(Integer, default=100)
 
     # 多対多リレーション
