983ebd3930b04bdd3512b78d3f3e1bd604383a12
sibapybot <sibabot.python@gmail.com>
2024-09-21 19:38:12 +0900

    :hammer: crad内処理の見直し

--- app/cruds/client.py
+++ app/cruds/client.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import Session
 from ..schemas import (
     Client as ClientSchema,
     UpdataClient as UpdataClientSchema,
+    User as UserSchema,
+    App as AppSchema,
 )
 from ..models import (
     Client as ClientModel,
@@ -22,59 +24,34 @@ async def get_client_by_id(
 
 
 async def create_client(
-        session: Session, client: ClientSchema,
-        user, app
+        session: Session, client: ClientModel,
+        user: UserSchema, app: AppSchema, user_client=True
         ) -> ClientSchema:
-    client = ClientModel(**client.model_dump())
-    db.add(client)
-    db.commit()
-    db.refresh(client)
-    return UserClientSchema.model_validate(client)
+    if user_client:
+        client.user = UserModel(**user.model_dump())
+    else:
+        client.app = AppModel(**app.model_dump())
+    session.add(client)
+    session.commit()
+    session.refresh(client)
+    return ClientSchema.model_validate(client)
 
 
 async def update_user_client(
             session: Session,
-            client: UserClientSchema,
-            updates: UserClientSchema):
-    existing_client = (
-        db.query(ClientModel)
-        .filter(ClientModel.id == client.id)
-        .first()
-        )
-    if existing_client:
-        user = db.query(UserModel).filter(UserModel.id == client.user).first()
-        if not user:
-            return None
-        existing_client.update(**client.model_dump(), user=user)
-        db.commit()
-        db.refresh(existing_client)
-        return UserClientSchema.model_validate(existing_client)
-    return None
+            client: ClientModel,
+            updates: UpdataClientSchema
+        ) -> ClientSchema:
+    update_data = updates.model_dump()
+    for key, value in update_data.items():
+        setattr(client, key, value)
+    session.commit()
+    return ClientSchema.model_validate(client)
 
 
-async def create_app_client(db: Session, client: AppClientSchema):
-    app = db.query(AppModel).filter(AppModel.id == client.app).first()
-    if not app:
-        return None
-    client = ClientModel(**client.model_dump(), app=app)
-    db.add(client)
-    db.commit()
-    db.refresh(client)
-    return AppClientSchema.model_validate(client)
-
-
-async def update_app_client(db: Session, client: AppClientSchema):
-    existing_client = (
-        db.query(ClientModel)
-        .filter(ClientModel.id == client.id)
-        .first()
-        )
-    if existing_client:
-        app = db.query(AppModel).filter(AppModel.id == client.app).first()
-        if not app:
-            return None
-        existing_client.update(**client.model_dump(), app=app)
-        db.commit()
-        db.refresh(existing_client)
-        return AppClientSchema.model_validate(existing_client)
-    return None
+async def delete_client(
+            session: Session, client: ClientModel
+        ) -> ClientSchema:
+    session.delete(client)
+    session.commit()
+    return ClientSchema.model_validate(client)
