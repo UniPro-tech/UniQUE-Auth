5de853b62e298ed9104aecd738cb0b328c559ae9
sibapybot <sibabot.python@gmail.com>
2024-10-25 22:14:11 +0900

    :sparkles: 中間テーブルの値を固有化

--- app/models/middle_table.py
+++ app/models/middle_table.py
@@ -5,43 +5,43 @@ from ..database import Base
 user_roles = Table(
     'user_roles',
     Base.metadata,
-    Column('user_id', Integer, ForeignKey('users.id')),
-    Column('role_id', Integer, ForeignKey('roles.id'))
+    Column('user_id', Integer, ForeignKey('users.id'), primary_key=True),
+    Column('role_id', Integer, ForeignKey('roles.id'), primary_key=True)
 )
 
 # UserとFlagの多対多リレーション
 user_flags = Table(
     'user_flags',
     Base.metadata,
-    Column('user_id', Integer, ForeignKey('users.id')),
-    Column('flag_id', Integer, ForeignKey('flags.id'))
+    Column('user_id', Integer, ForeignKey('users.id'), primary_key=True),
+    Column('flag_id', Integer, ForeignKey('flags.id'), primary_key=True)
 )
 
 # UserとAppの多対多リレーション
 user_apps = Table(
     'user_apps',
     Base.metadata,
-    Column('user_id', Integer, ForeignKey('users.id')),
-    Column('app_id', Integer, ForeignKey('apps.id'))
+    Column('user_id', Integer, ForeignKey('users.id'), primary_key=True),
+    Column('app_id', Integer, ForeignKey('apps.id'), primary_key=True)
 )
 
 app_admin_users = Table(
     "app_admin_users",
     Base.metadata,
-    Column("app_id", Integer, ForeignKey("apps.id")),
-    Column("user_id", Integer, ForeignKey("users.id"))
+    Column("app_id", Integer, ForeignKey("apps.id"), primary_key=True),
+    Column("user_id", Integer, ForeignKey("users.id"), primary_key=True)
 )
 
 flag_admin_users = Table(
     "flag_admin_users",
     Base.metadata,
-    Column("flag_id", Integer, ForeignKey("flags.id")),
-    Column("user_id", Integer, ForeignKey("users.id"))
+    Column("flag_id", Integer, ForeignKey("flags.id"), primary_key=True),
+    Column("user_id", Integer, ForeignKey("users.id"), primary_key=True)
 )
 
 flag_roles = Table(
     "flag_roles",
     Base.metadata,
-    Column("flag_id", Integer, ForeignKey("flags.id")),
-    Column("role_id", Integer, ForeignKey("roles.id"))
+    Column("flag_id", Integer, ForeignKey("flags.id"), primary_key=True),
+    Column("role_id", Integer, ForeignKey("roles.id"), primary_key=True)
 )
