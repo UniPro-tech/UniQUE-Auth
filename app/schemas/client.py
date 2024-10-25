5c26f7666ea8a4dbb966254d477dccd6557d3b18
sibapybot <sibabot.python@gmail.com>
2024-10-25 23:40:16 +0900

    :hammer: 値の情報を追加

--- app/schemas/client.py
+++ app/schemas/client.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class BaseClient(BaseModel):
@@ -12,10 +12,9 @@ class Client(BaseClient):
     """
     Clientの詳細クラス
     """
-    id: int
-    is_enable: bool
-    user: int
-    app: int
+    is_enable: bool = Field(default=True)
+    user: int = Field(description="ユーザーID")
+    app: int = Field(description="アプリケーションID")
 
     class Config:
         model_validate = True
