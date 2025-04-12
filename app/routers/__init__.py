6b10bee42c1591054933ae45b3ddeb61a60c0d4f
sibapybot <sibabot.python@gmail.com>
2025-04-12 23:24:13 +0900

    :sparkles: 必須パラメータの確認処理を追加

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,35 +1,59 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from typing import Optional
 from ..cruds.user import get_user_by_name, get_user_by_email, create_user
 from .. import schemas
 from ..database import get_db
 
 router = APIRouter(
-    prefix="/users",
+    prefix="/",
     tags=["users"],
     responses={404: {"description": "Not found"}},
 )
 
 
-@router.post("/", response_model=schemas.CreateUser)
-async def create_users(
-            user: schemas.CreateUser,
+class AuthorizeRequest(BaseModel):
+    """ユーザー登録リクエスト"""
+    scope: str  # REQUIRED
+    response_type: str  # REQUIRED
+    client_id: int  # REQUIRED
+    redirect_uri: str  # REQUIRED
+    state: str  # REQUIRED
+    response_mode: Optional[str]  # OPTIONAL
+    nonce: str  # OPTIONAL
+    display: str  # OPTIONAL
+    prompt: str  # OPTIONAL
+    max_age: int  # OPTIONAL
+    ui_locales: str  # OPTIONAL
+    id_token_hint: str  # OPTIONAL
+    login_hint: str  # OPTIONAL
+    acr_values: str  # OPTIONAL
+
+    def validate_required_fields(self) -> None:
+        """Check if REQUIRED fields are not None"""
+        required_fields = ["scope", "response_type", "client_id", "redirect_uri", "state"]
+        for field in required_fields:
+            if getattr(self, field) is None:
+                raise ValueError(f"The field '{field}' is required and cannot be None.")
+
+
+@router.post("/authorize", response_model=schemas.User)
+async def authorize(
+            request: AuthorizeRequest,
             db: Session = Depends(get_db)
         ):
-    user_check = get_user_by_email(db, user.email)
-    if user_check:
-        raise HTTPException(status_code=400, detail="Email already registered")
+    """
+    OIDC認証・認可リクエストエンドポイント
+    """
+    # リクエストパラメータの検証
+    try:
+        request.validate_required_fields()
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
-    user_check = get_user_by_name(db, user.name)
-    if user_check:
-        raise HTTPException(status_code=400, detail="Name already registered")
+    # ユーザーの認証
 
-    user_data = schemas.CreateUser(
-        name=user.name,
-        email=user.email,
-        passwd=user.hash_password
-    )
 
-    user = await create_user(user_data, db)
+    # ユーザーの認可& トークンの発行
 
-    return user
