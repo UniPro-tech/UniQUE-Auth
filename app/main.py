3af5e80c29ff39e0fc788dda14b1b9fc179f5890
sibapybot <sibabot.python@gmail.com>
2025-04-19 00:01:22 +0900

    :zap: miniサイズに不要な箇所を削除

--- app/main.py
+++ app/main.py
@@ -1,54 +1,20 @@
 from fastapi import FastAPI
-from app.routers import apps, flags, users, token, roles
-from app.routers.apps import (
-    users as apps_users,
-    list as apps_list
-    )
-from app.routers.flags import (
-    users as flags_users,
-    list as flags_list,
-    roles as flags_roles
-    )
-from app.routers.roles import (
-    list as roles_list
-    )
-from app.routers.token import code
-from app.routers.users import (
-    me as users_me,
-    invite as users_invite,
-    list as users_list,
-    flags as users_flags,
-    link_apps as users_link_apps
-)
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.middleware import Middleware
+from app import routers
 from app.database import engine, Base
 
 
 # Create the database tables
 Base.metadata.create_all(bind=engine)
 
+
+middleware = [
+    Middleware(SessionMiddleware, secret_key="your-secret-key")
+]
+
 app = FastAPI(
     root_path="/api/v1"
 )
 
-# apps配下
-app.include_router(apps.router)
-app.include_router(apps_list.router)
-app.include_router(apps_users.router)
-
-app.include_router(flags.router)
-app.include_router(flags_users.router)
-app.include_router(flags_list.router)
-app.include_router(flags_roles.router)
-
-app.include_router(roles.router)
-app.include_router(roles_list.router)
-
-app.include_router(token.router)
-app.include_router(code.router)
-
-app.include_router(users.router)
-app.include_router(users_me.router)
-app.include_router(users_invite.router)
-app.include_router(users_list.router)
-app.include_router(users_flags.router)
-app.include_router(users_link_apps.router)
+app.include_router(routers)
