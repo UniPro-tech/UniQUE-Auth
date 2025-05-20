131d0c10e3a06bed2e5e4117881387f035ea2d2d
sibapybot <sibabot.python@gmail.com>
2025-05-20 21:38:52 +0900

    :memo: 自分のためにドキュメントを追加

--- models/token.py
+++ models/token.py
@@ -15,6 +15,27 @@ class TokenStatus(enum.Enum):
 
 # アクセストークン及び、リフレッシュトークンにはtoken_hashを保存しセキュリティを考慮する
 class AccessToken(Base):
+    """
+    アクセストークンのモデルクラス
+    id: int
+        アクセストークンのID
+    token_hash: str
+        アクセストークンのハッシュ値
+    sub: str
+        ユーザーのID
+    iss: str
+        レスポンスを返した Issuer の Issuer Identifier。httpsスキーマで始まるURL
+    client_id: str
+        クライアントのID
+    scope: str
+        アクセストークンのスコープ
+    iat: datetime
+        発行日時
+    exp: datetime
+        有効期限
+    status: TokenStatus
+        トークンの状態（active, revoked, expired）
+    """
     __tablename__ = "access_tokens"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
@@ -29,6 +50,31 @@ class AccessToken(Base):
 
 
 class IDToken(Base):
+    """
+    IDトークンのモデルクラス
+    id: int
+        ID（管理用）
+    sub: str
+        ユーザーのID
+    iss: str
+        レスポンスを返した Issuer の Issuer Identifier。httpsスキーマで始まるURL
+    aud: str
+        client_id
+    iat: datetime
+        発行日時
+    exp: datetime
+        有効期限
+    auth_time: datetime
+        認証時刻
+    nonce: str
+        認証リクエストに含まれるnonce
+    acr: str
+        認証要求に含まれる認証方式
+    amr: str
+        認証要求に含まれる認証方法
+    azp: str
+        認可されたクライアントのID
+    """
     __tablename__ = "id_tokens"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
@@ -39,9 +85,35 @@ class IDToken(Base):
     exp: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
     auth_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
     nonce: Mapped[str] = mapped_column(String(255), nullable=True)
+    acr: Mapped[str] = mapped_column(String(255), nullable=True)
+    amr: Mapped[str] = mapped_column(String(255), nullable=True)
+    azp: Mapped[str] = mapped_column(String(255), nullable=True)
 
 
 class RefreshToken(Base):
+    """
+    リフレッシュトークンのモデルクラス
+    id: int
+        リフレッシュトークンのID
+    token_hash: str
+        リフレッシュトークンのハッシュ値
+    sub: str
+        ユーザーのID
+    iss: str
+        レスポンスを返した Issuer の Issuer Identifier。httpsスキーマで始まるURL
+    client_id: str
+        クライアントのID
+    scope: str
+        アクセストークンのスコープ
+    iat: datetime
+        発行日時
+    exp: datetime
+        有効期限
+    status: TokenStatus
+        トークンの状態（active, revoked, expired）
+    rotated_from_id: int
+        リフレッシュトークンのローテーション元のID(追跡用)
+    """
     __tablename__ = "refresh_tokens"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
