4022dace4677053963f6c0d06e8fd727c019592b
sibapybot <sibabot.python@gmail.com>
2025-05-21 20:59:58 +0900

    :memo: テスト機能の候補を追加

--- tests/crud.py
+++ tests/crud.py
@@ -27,17 +27,16 @@ from cruds.session import (
 )
 from cruds.token import (
     create_access_token,
-    get_access_token_by_id,
-    update_access_token,
-    delete_access_token_by_id,
+    get_access_token_by_hash,
+    update_access_token_status,
+    delete_access_token_by_hash,
     create_refresh_token,
-    get_refresh_token_by_id,
-    update_refresh_token,
-    delete_refresh_token_by_id,
+    get_refresh_token_by_hash,
+    update_refresh_token_status,
+    delete_refresh_token_by_hash,
     create_id_token,
-    get_id_token_by_id,
-    update_id_token,
-    delete_id_token_by_id
+    get_id_token_by_sub,
+    delete_id_token_by_hash
 )
 
 session = next(get_db())
@@ -81,3 +80,82 @@ def setup_module(module):
     ]
     for client in client_list:
         create_client(session, **client)
+
+
+# userのCRUDテスト
+# 1. 作成
+
+# 2. 検索
+
+# 3. 更新
+
+# 4. 削除
+
+# 5. useridが存在しない場合の処理
+
+# 6. ユーザー名の重複チェック
+
+# 7. メールアドレスの重複チェック
+
+# 8. パスワードのハッシュ化と検証
+
+# 9. ユーザーデータの更新時間が正しく更新されること
+
+# 10. ユーザーの削除が正しく行われること
+
+
+# clientのCRUDテスト
+# 1. 作成
+
+# 2. 検索
+
+# 3. 更新
+
+# 4. 削除
+
+# 5. client_idが存在しない場合の処理
+
+# 6. client_idの重複チェック
+
+# 7. リダイレクトURIの作成
+
+# 8. リダイレクトURIの削除
+
+# 9. リダイレクトURIの重複チェック
+
+# 10. クライアントデータの更新時間が正しく更新されること
+
+# 11. クライアントの削除が正しく行われること
+
+# 12. クライアントが削除された場合、関連するリダイレクトURIも削除されること
+
+# sessionのCRUDテスト
+# 1. 作成
+
+# 2. 検索
+
+# 3. 削除
+
+# 4. session_idが存在しない場合の処理
+
+# 5. セッションの作成時に関連するクライアントとユーザーが正しく設定されること
+
+# 6. セッションの削除が正しく行われること
+
+# 7. セッションの更新時間が正しく更新されること
+
+# 8. セッションの削除が正しく行われること
+
+# 9. ユーザーが削除された場合、関連するセッションも削除されること
+
+# 10. クライアントが削除された場合、関連するセッションも削除されること
+
+
+# tokenのCRUDテスト
+# 1. 作成
+
+# 2. 検索
+
+# 3. 更新
+
+# 4. 削除
