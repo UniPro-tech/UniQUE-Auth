88d118fbf22be7a05dca9909f78f7ae5ec3c209a
sibapybot <sibabot.python@gmail.com>
2025-05-23 21:54:16 +0900

    :hammer: 必須パラメータの確認まで

--- cruds/client.py
+++ cruds/client.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from datetime import datetime
 from zoneinfo import ZoneInfo
-from models.session import Session as SessionModel
+# from models.session import Session as SessionModel
 from models.client import Client, Redirect_URI
 
 
