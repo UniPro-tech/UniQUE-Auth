3f7b9641241568c12dd40c8f5ab7e2b66f4179b9
sibapybot <sibabot.python@gmail.com>
2025-05-24 19:29:03 +0900

    :sparkles: DBのセッションテーブルに保存する値を追加

--- models/session.py
+++ models/session.py
@@ -20,7 +20,17 @@ class Session(Base):
     __tablename__ = "sessions"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)  # セッションID
-    session_id: Mapped[str] = mapped_column(String(255), unique=True, index=True)  # ユニークなセッションID
+    session_id: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)  # セッションID
+    auth_time: Mapped[datetime] = mapped_column(DateTime, nullable=False)  # 認証日時
+    scope: Mapped[str] = mapped_column(String(255), nullable=False)  # スコープ
+    acr: Mapped[str] = mapped_column(String(255), nullable=False)  # 認証レベル
+    amr: Mapped[str] = mapped_column(String(255), nullable=False)  # 認証方法
+    nonce: Mapped[str] = mapped_column(String(255), nullable=False)  # nonce
+
+    ip_address: Mapped[str] = mapped_column(String(45), nullable=False)  # IPアドレス
+    user_agent: Mapped[str] = mapped_column(String(255), nullable=False)  # ユーザーエージェント
+
+    revoked: Mapped[bool] = mapped_column(default=False, nullable=False)  # セッションが取り消されたかどうか
 
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(ZoneInfo("UTC")))  # 作成日時
     updated_at: Mapped[datetime] = mapped_column(
