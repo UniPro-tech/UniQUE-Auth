f587062f2379d78971ee256ac7ed1f01abac10f2
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-07-18 15:58:09 +0900

    model.pyの変更に追従

--- unique_api/app/data.py
+++ unique_api/app/data.py
@@ -1,15 +1,14 @@
 # テスト用のデータを注入する
 from sqlalchemy.orm import Session
-from unique_api.app.model import Member, User, App, RedirectURI
-from datetime import datetime
+from unique_api.app.model import Users, Apps, RedirectUris
 import hashlib
 
 
 def create_test_data(db: Session):
     # アプリケーションの作成 (存在しない場合のみ)
-    app = db.query(App).filter_by(client_id="admin_app").first()
+    app = db.query(Apps).filter_by(client_id="admin_app").first()
     if not app:
-        app = App(
+        app = Apps(
             client_id="admin_app",
             client_secret="password",
             name="アドミンアプリケーション",
@@ -20,43 +19,30 @@ def create_test_data(db: Session):
 
     # リダイレクトURIの作成 (存在しない場合のみ)
     redirect_uri = (
-        db.query(RedirectURI)
+        db.query(RedirectUris)
         .filter_by(uri="https://example.com/callback", app_id=app.id)
         .first()
     )
     if not redirect_uri:
-        redirect_uri = RedirectURI(app_id=app.id, uri="https://example.com/callback")
+        redirect_uri = RedirectUris(app_id=app.id, uri="https://example.com/callback")
         db.add(redirect_uri)
 
-    # メンバーの作成 (存在しない場合のみ)
-    member = db.query(Member).filter_by(custom_id="admin").first()
-    if not member:
-        member = Member(
-            custom_id="admin",
-            name="admin",
-            email="admin@example.com",
-            external_email="admin@example.com",
-            updated_at=datetime.now(),
-            joined_at=datetime.now(),
-            is_enable=True,
-            period="00",
-            system=False,
-        )
-        db.add(member)
-
     # ユーザーの作成 (存在しない場合のみ)
-    user = db.query(User).filter_by(custom_id="admin").first()
+    user = db.query(Users).filter_by(custom_id="admin").first()
     if not user:
-        user = User(
+        user = Users(
             custom_id="admin",
             password_hash=hashlib.sha256("admin".encode()).hexdigest(),
-            is_enable=False,
+            name="管理者",
+            email="admin@uniproject.jp",
+            external_email="admin@uniproject.jp",
+            is_enable=True,
+            is_system=True,
+            period="00",
         )
         db.add(user)
         db.flush()  # ユーザーを先に保存してIDを取得
 
-    # TODO: ユーザーのメールアドレスの登録ロジックを変更
-    # db.add(email)
     db.commit()  # リダイレクトURIを保存
     # テストユーザーで認可する際のテストURLを生成
     print(
