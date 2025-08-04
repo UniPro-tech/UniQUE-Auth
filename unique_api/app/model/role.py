d67f4618c0664af0a38d4b18eb7bb3d5cc12509d
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-08-04 15:36:13 +0900

    カラム名変更に伴うmodelの変更

--- unique_api/app/model/role.py
+++ unique_api/app/model/role.py
@@ -11,6 +11,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
 from unique_api.app.db import Base
 from unique_api.app.model.util import generate_ulid
+
 if TYPE_CHECKING:
     from unique_api.app.model.intermediate import UserRole
 
@@ -25,12 +26,15 @@ class Roles(Base):
     id: Mapped[str] = mapped_column(
         String(255, "utf8mb4_unicode_ci"), primary_key=True, default=generate_ulid
     )
+    name: Mapped[Optional[str]] = mapped_column(
+        String(255, "utf8mb4_unicode_ci"), nullable=True
+    )
     permissions: Mapped[int] = mapped_column(Integer, server_default=text("'0'"))
     created_at: Mapped[datetime] = mapped_column(
         TIMESTAMP, server_default=text("CURRENT_TIMESTAMP")
     )
     is_enable: Mapped[int] = mapped_column(TINYINT(1), server_default=text("'1'"))
-    system: Mapped[int] = mapped_column(TINYINT(1), server_default=text("'0'"))
+    is_system: Mapped[int] = mapped_column(TINYINT(1), server_default=text("'0'"))
     custom_id: Mapped[Optional[str]] = mapped_column(String(255, "utf8mb4_unicode_ci"))
     updated_at: Mapped[Optional[datetime]] = mapped_column(TIMESTAMP)
 
