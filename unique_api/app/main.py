fd10133b33398cc1942f7dff847a88f859866d44
sibapybot <sibabot.python@gmail.com>
2025-08-05 21:07:45 +0900

    :hammer: 環境変数の読み込みを追加

--- unique_api/app/main.py
+++ unique_api/app/main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from starlette.middleware.sessions import SessionMiddleware
 import uvicorn
+from dotenv import load_dotenv
 
 from db import engine, Base, get_db
 from unique_api.app.router.authorization import router as authorization_router
 from unique_api.app.router.authentication import router as authentication_router
+
+load_dotenv('.env')
 
 # データベースをリセット
 # Base.metadata.drop_all(bind=engine)
