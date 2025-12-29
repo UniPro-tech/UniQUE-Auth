7d17deef76cdce0511f74e55a7cf8cd140a07b77
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-12-29 23:01:55 +0900

    envからデータベースのURLを取得する

--- unique_api/app/db.py
+++ unique_api/app/db.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql://api:P%40ssw0rd@unique_db:3306/devdb"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
