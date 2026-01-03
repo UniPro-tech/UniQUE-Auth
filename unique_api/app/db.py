d06d1fff47ba985259430424e92099fb1ec7234d
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-01-03 11:58:57 +0900

    データベースのメタデータ命名規則を追加し、TokenSetsモデルのForeignKeyConstraintを簡略化

--- unique_api/app/db.py
+++ unique_api/app/db.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
@@ -9,7 +9,17 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadata = MetaData(naming_convention=convention)
+
+Base = declarative_base(metadata=metadata)
 
 
 def get_db():
