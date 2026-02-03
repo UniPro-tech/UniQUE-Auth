f84df29fe50960c835796517d68deff4db0863c9
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-02-04 08:17:12 +0900

    add consented request

---
+++ src/internal/router/consented.go
@@ -0,0 +1,56 @@
+package router
+
+import (
+	"github.com/UniPro-tech/UniQUE-Auth/internal/query"
+	"github.com/gin-gonic/gin"
+)
+
+type ConsentedGetRequest struct {
+	AuthorizationID string `form:"authorization_id" binding:"required"`
+}
+
+// ConsentedGet godoc
+// @Summary Redirect for Authorization Request
+// @Schemes
+// @Description 認可リクエストが完了したのちにリダイレクトするためのエンドポイントです。
+// @Tags authorization
+// @Accept json
+// @Produce json
+// @Param authorization_id query string true "Authorization Request ID"
+// @Success 301 {string} string "Redirect to client application with authorization code"
+// @Router /consented [get]
+func ConsentedGet(c *gin.Context) {
+	req := ConsentedGetRequest{}
+	if err := c.ShouldBindQuery(&req); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	authReq, err := query.AuthorizationRequest.Where(query.AuthorizationRequest.ID.Eq(req.AuthorizationID)).First()
+	if authReq == nil || !authReq.IsConsented {
+		c.JSON(400, gin.H{"error": "invalid auth_request_id"})
+		return
+	}
+	if err != nil {
+		c.JSON(500, gin.H{"error": "internal server error"})
+		return
+	}
+
+	// Generate authorization code or token based on response_type
+	switch authReq.ResponseType {
+	case "code":
+		if authReq.Code == "" {
+			c.JSON(400, gin.H{"error": "not check consented"})
+			return
+		}
+		// Redirect to client's redirect_uri with authorization code and state
+		redirectURL := authReq.RedirectURI + "?code=" + authReq.Code
+		if authReq.State != "" {
+			redirectURL += "&state=" + authReq.State
+		}
+		c.Redirect(301, redirectURL)
+		return
+	default:
+		c.JSON(400, gin.H{"error": "unsupported response_type"})
+		return
+	}
+}
