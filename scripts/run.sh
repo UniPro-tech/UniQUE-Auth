c386e079751e8c99166928eb4253712f0e6ebd49
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-02-06 23:36:28 +0900

    dockerを整備

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ ! -f /app/keys/rsa_private.pem ] || [ ! -f /app/keys/rsa_public.pem ]; then \
-  openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out /app/keys/rsa_private.pem && \
-  openssl rsa -pubout -in /app/keys/rsa_private.pem -out /app/keys/rsa_public.pem && \
-  chmod 600 /app/keys/rsa_private.pem && chmod 644 /app/keys/rsa_public.pem; \
-fi
+if [ ! -f /app/keys/private/rsa_private.pem ] || [ ! -f /app/keys/public/rsa_public.pem ]; then \
+    openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out /app/keys/private/rsa_private.pem && \
+    openssl rsa -pubout -in /app/keys/private/rsa_private.pem -out /app/keys/public/rsa_public.pem && \
+    chmod 600 /app/keys/private/rsa_private.pem && chmod 644 /app/keys/public/rsa_public.pem; \
+  fi
 
-uv run --active unique_api/app/main.py
+/app/server
